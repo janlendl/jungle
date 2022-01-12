a6ff0165b518b723d9d87c1df3ca51b6bd67ab4c
Lendl Peralta <janlendl@gmail.com>
2022-01-12 22:02:09 +0000

    Add basic auth for dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,17 @@
 class Admin::DashboardController < ApplicationController
+
+  before_filter :authenticate
+
   def show
   end
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end
+
+  
 end
