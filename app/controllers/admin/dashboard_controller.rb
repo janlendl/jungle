c8c195f038fe54d0c3e298967b525e909daa39a7
Lendl Peralta <janlendl@gmail.com>
2022-01-13 01:55:58 +0000

    Add instance variable for product and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ class Admin::DashboardController < ApplicationController
   before_filter :authenticate
 
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 
   protected
