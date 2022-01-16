fda3d3b6d28a40d2aba779f78681dcabc7c0e014
Lendl Peralta <janlendl@gmail.com>
2022-01-16 05:29:40 +0000

    Add comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  has_secure_password
+  has_secure_password   # authentication method via bcrypt
 
   validates :name, presence: true
   validates :email, presence: true
