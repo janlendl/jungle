303744683edfcc55287fb39c1164376dfde4bde9
Lendl Peralta <janlendl@gmail.com>
2022-01-16 06:32:45 +0000

    Attempting to find error when signup link is clicked

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
+
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
@@ -16,7 +18,11 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(
+      :name, 
+      :email, 
+      :password, 
+      :password_confirmation)
   end
 
 end
