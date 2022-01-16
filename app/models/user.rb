4996903d3948bf8646095197fa74a5db60e52fe8
Lendl Peralta <janlendl@gmail.com>
2022-01-16 05:26:46 +0000

    Define user requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
