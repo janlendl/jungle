c509c35912ffd7186779875e031a89e9edc97dcb
Lendl Peralta <janlendl@gmail.com>
2022-01-26 07:43:19 +0000

    Add more test for price, quantity and category

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -11,12 +11,34 @@ RSpec.describe Product, type: :model do
 
       expect(@product.id).to be_present
     end
-
+    
     it 'should not save if the name is empty' do
       @category = Category.new(name: "My Category")
       @category.save!
       @product = Product.new(name: nil, price: 100, quantity: 2, category: @category)
- 
+  
+      expect { @product.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'should not save if the price is empty' do
+      @category = Category.new(name: "My Category")
+      @category.save!
+      @product = Product.new(name: "My Product", price: nil, quantity: 2, category: @category)
+  
+      expect { @product.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'should not save if the quantity is empty' do
+      @category = Category.new(name: "My Category")
+      @category.save!
+      @product = Product.new(name: "My Product", price: 5000, quantity: nil, category: @category)
+  
+      expect { @product.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'should not save if the category is empty' do
+      @product = Product.new(name: "My Product", price: 5000, quantity: 2, category: nil)
+  
       expect { @product.save! }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
