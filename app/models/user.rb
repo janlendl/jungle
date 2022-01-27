3bdb899b08edc22008e774e37ec47bc2db812134
Lendl Peralta <janlendl@gmail.com>
2022-01-27 04:35:02 +0000

    update active model validation to reflect password min length, password confirmation, email case sensitivity

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password   # authentication method via bcrypt
 
   validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
+  before_save { self.email.downcase! }
 end
