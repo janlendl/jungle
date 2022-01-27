b7abf7b421b994c95e0b4f3267b3befd9e0362fd
Lendl Peralta <janlendl@gmail.com>
2022-01-27 05:05:32 +0000

    Add check for blank password and password_confirmation, password length check

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,28 @@ RSpec.describe User, type: :model do
 
   describe 'Validations' do
     it 'should be valid with matching password' do
-      @user = User.new(name: "John Wick", email: "john@wick.com", password: "123", password_confirmation: "123")
-      expect(@user.password).to match(@user.password_confirmation)
+      @user = User.new(name: "John Wick", email: "john@wick.com", password: "matchy", password_confirmation: "matchy")
+      expect(@user).to be_valid
     end
 
     it 'should be invalid if the password does not match' do
-      @user = User.new(name: "John Wick", email: "john@wick.com", password: "123", password_confirmation: "1333")
-      expect(@user.password).to_not match(@user.password_confirmation)
+      @user = User.new(name: "John Wick", email: "john@wick.com", password: "12345678", password_confirmation: "1333")
+      expect(@user).to_not be_valid
+    end
+
+    it 'should be invalid if the password is blank' do
+      @user = User.new(name: "John Wick", email: "john@wick.com", password: nil, password_confirmation: "133322")
+      expect(@user).to_not be_valid
+    end
+
+    it 'should be invalid if the password_confirmation is blank' do
+      @user = User.new(name: "John Wick", email: "john@wick.com", password: "123456", password_confirmation: nil)
+      expect(@user).to_not be_valid
+    end
+
+    it 'should meet the password minimum length of 6' do
+      @user = User.new(name: "John Wick", email: "john@wick.com", password: "123456", password_confirmation: "123456")
+      expect(@user).to be_valid
     end
 
     it 'should be invalid if name is blank' do
@@ -22,7 +37,14 @@ RSpec.describe User, type: :model do
       @user = User.new(name: "Mr. Anderson", email: nil, password: "123", password_confirmation: "123")
       expect { @user.save! }.to raise_error(ActiveRecord::RecordInvalid)
     end
-  end
 
+    # it 'should be invalid if email already exists' do
+    #   user.email = "this@myemail.com"
+
+    #   @user = User.new(name: "Mr. Anderson", email: "this@myemail.com", password: "123456", password_confirmation: "123456")
+    #   expect(@user).to be_valid
+    # end
+
+  end
 
 end
