d8870023f3a1def7b996aa804f570b7fdf118087
Lendl Peralta <janlendl@gmail.com>
2022-01-28 04:04:56 +0000

    Add downcase to find by email function. Add self on the method declaration for me to be able to call the User class in the user rspec file

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true, length: { minimum: 6 }
-  before_save { self.email.downcase! }
 
-  def authenticate_with_credentials(email, password)
-    user = User.find_by_email(email.strip)
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
     if user && user.authenticate(password)
       return user
     else
