cc32c67bf436a854e94a16c251a5e70f23deb903
Lendl Peralta <janlendl@gmail.com>
2022-01-30 20:20:49 +0000

    Complete add to cart spec

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor can add product to cart", type: :feature, js: true  do
+  
+  #SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the product details" do
+  
+    # ACT
+    visit root_path
+    first('.product').click_on('Add')
+
+    #DEBUG / VERIFY WITH SLEEP TIMER
+    sleep(2)
+    save_screenshot 'test_add_to_cart.png'
+
+    expect(page).to have_content('My Cart (1)')
+  end
+end
