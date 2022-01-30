a41f91c137dfd1515a40f371c09ffdb0a89881de
Lendl Peralta <janlendl@gmail.com>
2022-01-30 20:01:47 +0000

    Complete feature test for clicking a particular product to view the details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor goes to the product detail page", type: :feature, js: true  do
+  
+  #SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the product details" do
+  
+    # ACT
+    visit root_path
+    first('.product > header').click
+
+    #DEBUG / VERIFY WITH SLEEP TIMER
+    sleep(2)
+    save_screenshot
+
+    expect(page).to have_css 'article.product-detail'
+  end
+end
